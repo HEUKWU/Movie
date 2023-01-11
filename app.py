815e570cd81ea9c9c607119b62929c675ba00506
HEUKWU <yoo4068@gmail.com>
2023-01-11 23:44:55 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.dq4uizr.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    image = og_image['content']
+    title = og_title['content']
+    description = og_description['content']
+    count = list(db.movies.find({},{'_id':False}))
+    num = len(count) + 1
+
+    doc = {
+        'image':image,
+        'title':title,
+        'desc':description,
+        'star':star_receive,
+        'comment':comment_receive,
+        'num': num
+    }
+
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg':'POST 연결 완료!'})
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movies_list = list(db.movies.find({},{'_id':False}))
+    return jsonify({'movies':movies_list})
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    num_receive = request.form['num_give']
+    db.movies.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5002, debug=True)
